8277ba01ce4c901b273ef2d38c5ae68b0a1d4485
Erin Ringland <erinringland@gmail.com>
2020-12-04 01:07:34 -0600

    Removed previously installed npm plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,24 @@
-const {src, dest} = require('gulp');
+// const {src, dest} = require('gulp');
+// const {src, dest} = require('gulp');
+
+
+const { src, dest, series, parallel } = require ('gulp');
 
 function move(){
   return src('src/**/*')
   .pipe(dest('dest/'))
 }
 
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
+
+function stylesTask() {
+  return src('src/css/*.css')
+  .pipe(postcss([ autoprefixer() ]))
+  .pipe(dest('/dest/css'))
+}
+
 exports.move = move;
+exports.styles = stylesTask;
+
