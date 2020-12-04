59525a3bdf315203fcf4b962ed0ed43ed8ad86c6
Erin Ringland <erinringland@gmail.com>
2020-12-04 01:41:05 -0600

    Deleted gulp-concat-css and added task for styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const gulp = require('gulp');
 const csso = require('gulp-csso');
-const concatCss = require('gulp-concat-css');
 const sourcemaps = require('gulp-sourcemaps');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify-es').default;
@@ -9,4 +8,13 @@ const imagemin = require('gulp-imagemin');
 gulp.task('move', function () {
   return gulp.src('src/*')
       .pipe(gulp.dest('dest/'));
+});
+
+gulp.task('styles', function () {
+  return gulp.src('src/css/*')
+    .pipe(concat('style.css'))
+    .pipe(sourcemaps.init())
+    .pipe(csso())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dest/css/'));
 });
